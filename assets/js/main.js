00428d0d6fc96d46acd94956b4d31afc5c1c2441
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-05-20 16:27:52 +0530

    header scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,32 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
 // scroll section active links
+const sections = document.querySelectorAll('selections[id]')
+window.addEventListener('scroll', scrollActive)
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+        } else {
+            document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
+
+        }
+    })
+}
 
 // change color header
+window.onscroll = ()=>{
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+
