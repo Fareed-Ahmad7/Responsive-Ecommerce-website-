944d94b2f2957b75cf3ee5d5b622dce5733593af
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-05-20 23:25:08 +0530

    featured section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // menu show
-const showMenu = (toggleId,navId)=>{
+const showMenu = (toggleId, navId)=>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
@@ -22,7 +22,8 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // scroll section active links
-const sections = document.querySelectorAll('selections[id]')
+const sections = document.querySelectorAll('section[id]')
+
 window.addEventListener('scroll', scrollActive)
 
 function scrollActive(){
@@ -33,18 +34,18 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){ 
+            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
         } else {
-            document.querySelectorAll('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
         }
     })
 }
 
 // change color header
 window.onscroll = ()=>{
-    const nav = document.getElementById('header')
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    const nav = document.getElementById('header');
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
 
 
